ca40a79087e26400c56162fc6536cf11541966a6
Zach Parris <Zach Parris@DESKTOP-CQMSADN>
2017-04-07 23:16:04 -0500

    Added DAL and Models

---
+++ ComicBookGallery/DAL/ComicBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.DAL
+{
+    public class ComicBookRepository
+    {
+        public ComicBook GetComicBook()
+        {
+            return null;
+        }
+    }
+}
